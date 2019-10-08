b4bd0033df48d9f06efa0defab206f2cfa4a994b
Arya Ziai <arya721@gmail.com>
2019-10-08 22:18:47 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,5 +12,5 @@
   end
 
   def introduction_with_language_optional(name, language)
-    puts "Hi, my name is Edwin and I am learning to program in #{language}."
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
   end
